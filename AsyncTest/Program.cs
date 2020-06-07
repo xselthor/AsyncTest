714db7e5c0f30b202baa1e5b53098984b12ce393
Patryk <patrykkrawczyk@gmail.com>
2020-06-07 10:45:47 +0200

    Add project files.

---
+++ AsyncTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AGetGWB();
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        public static async Task AGetGWB()
+        {
+            HttpClient hc = new HttpClient();
+            await hc.GetAsync("http://geekswithblogs.net/Default.aspx");
+            Console.WriteLine("Pobralem  zawartosc asynchronicznie");
+        }
+
+        public static void SGetGWB()
+        {
+            HttpClient hc = new HttpClient();
+            hc.GetAsync("http://geekswithblogs.net/Default.aspx");
+            Console.WriteLine("Pobralem zawartosc synchronicznie");
+        }
+    }
+}
